62e8ec64da2c2a9059ff5084576dba1e4ee06dfb
Criticyl <criticyl@gmail.com>
2025-12-13 21:53:32 +0000

    Added in a Window class to abstract it out from Main.cpp

---
+++ src/Core/Window.cpp
@@ -0,0 +1,102 @@
+#include "Window.h"
+
+namespace Grove {
+
+    Window::Window(int width, int height, const std::string& title)
+        : m_Width(width), m_Height(height), m_Title(title), m_Window(nullptr)
+    {
+        m_LastMouseX = width / 2.0f;
+        m_LastMouseY = height / 2.0f;
+        m_FirstMouse = true;
+        m_MouseOffsetX = 0.0f;
+        m_MouseOffsetY = 0.0f
+    }
+
+    Window::~Window() {
+        if (m_Window) {
+            glfwDestroyWindow(m_Window);
+        }
+        glfwTerminate();
+    }
+
+    void Window::init() {
+
+        if (!glfwInit()) {
+            std::cerr << "[ERROR] Failed to initialize GLFW" << std::endl;
+            return -1;
+        }
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        GLFWwindow* window = glfwCreateWindow(m_Width, m_Height, m_Title.c_str(), NULL, NULL);
+        if (window == NULL) {
+            std::cerr << "[ERROR] Failed to create GLFW window" << std::endl;
+            glfwTerminate();
+            return -1;
+        }
+        glfwMakeContextCurrent(window);
+
+        glfwSetWindowUserPointer(m_Window, this);
+
+        glfwSetFramebufferSizeCallback(m_Window, s_framebufferSizeCallback);
+        glfwSetCursorPosCallback(m_Window, s_mouseCallback);
+
+        glfwSetInputMode(m_Window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+            std::cerr << "[ERROR] Failed to initialize GLAD" << std::endl;
+            return;
+        }
+
+        glEnable(GL_DEPTH_TEST);
+    }
+
+    void Window::update() {
+        glfwSwapBuffers(m_Window);
+        glfwPollEvents();
+    }
+
+    bool Window::shouldClose() const {
+        return glfwWindowShouldClose(m_Window);
+    }
+
+    void Window::s_framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+        Window* instance = (Window*)glfwGetWindowUserPointer(window);
+        if (instance) {
+            instance->onResize(width, height);
+        }
+    }
+
+    void Window::s_mouseCallback(GLFWwindow* window, double xPos, double yPos) {
+        Window* instance = (Window*)glfwGetWindowUserPointer(window);
+        if (instance) {
+            instance->onMouseMove(xPos, yPos);
+        }
+    }
+
+    void Window::onResize(int width, int height) {
+        m_Width = width;
+        m_Height = height;
+        glViewport(0, 0, width, height);
+    }
+
+    void Window::onMouseMove(double xPos, double yPos) {
+        if (m_FirstMouse) {
+            m_LastMouseX = xPos;
+            m_LastMouseY = yPos;
+            m_FirstMouse = false;
+        }
+
+        float xOffset = xPos - m_LastMouseX;
+        float yOffset = m_LastMouseY - yPos;
+
+        m_LastMouseX = xPos;
+        m_LastMouseY = yPos;
+
+        m_MouseOffsetX = xOffset;
+        m_MouseOffsetY = yOffset;
+    }
+
+}
---
+++ src/Core/Window.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <string>
+#include <iostream>
+
+namespace Grove {
+    class Window {
+    public:
+        Window(int width, int height, const std::string& title);
+        ~Window();
+
+        void init();
+        void update();
+
+        bool shouldClose() const { return glfwWindowShouldClose(m_Window); }
+
+        GLFWwindow* getNativeWindow() const { return m_Window; }
+        int getWidth() const { return m_Width; }
+        int getHeight() const { return m_Height; }
+
+        float getMouseXOffset() {
+            float temp = m_MouseOffsetX;
+            m_MouseOffsetX = 0;
+            return temp;
+        }
+
+        float getMouseYOffset() {
+            float temp = m_MouseOffsetY;
+            m_MouseOffsetY;
+            return temp;
+        }
+
+    private:
+
+        static void s_framebufferSizeCallback(GLFWwindow* window, int width, int height);
+        static void s_mouseCallback(GLFWwindow* window, double xPos, double yPos);
+
+    private:
+
+        void onResize(int width, int height);
+        void onMouseMove(double xPos, double yPos);
+
+    private:
+
+        GLFWwindow* m_Window;
+        int m_Width, m_Height;
+        std::string m_Title;
+
+        float m_LastMouseX, m_LastMouseY;
+        float m_MouseOffsetX, m_MouseOffsetY;
+        bool m_FirstMouse;
+
+    };
+}
